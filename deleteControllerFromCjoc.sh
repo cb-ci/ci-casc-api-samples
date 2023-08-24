9ad4c02b794230760bda5ce5c4b5f9814dac76cb
andreas <caternberg@gmx.de>
2023-08-24 13:36:19 +0200

    add

---
+++ deleteControllerFromCjoc.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+source ./envvars.sh
+export CONTROLLER_NAME=${1:-$CONTROLLER_NAME}
+export CONTROLLER_URL=${BASE_URL}"/"${CONTROLLER_NAME}
+PATH_MANAGED_CONTROLLER="job/$CONTROLLER_NAME"
+PATH_TEAM_CONTROLLER="job/Teams/job/$CONTROLLER_NAME"
+#For Managed Controllers use this path
+PATH_CONTROLLER=$PATH_MANAGED_CONTROLLER
+#For Team Controllers use this path
+#PATH_CONTROLLER=$PATH_TEAM_CONTROLLER
+
+
+echo "force stop Controller $CONTROLLER_NAME"
+curl  -v -XPOST  -u $TOKEN "$CJOC_URL/$PATH_CONTROLLER/stopAction"
+
+sleep 10
+
+echo "delete Controller $CONTROLLER_NAME"
+curl  -v -XPOST -u $TOKEN "$CJOC_URL/$PATH_CONTROLLER/doDelete"
+
