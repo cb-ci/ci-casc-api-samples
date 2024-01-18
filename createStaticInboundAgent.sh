2ac1b9cf4578be6bd3e1f225b307d8b86f3cd2cd
cccaternberg <acaternberg@cloudbees.com>
2024-01-18 04:09:39 +0100

    update

--- createStaticInboundAgent.sh
+++ createStaticInboundAgent.sh
@@ -1,27 +1,32 @@
 #! /bin/bash
-AGENT_NAME="inbound4"
-TOKEN="user:token"
-HOST="https://CONTROLLER"
+AGENT_NAME="$(hostname)-1"
+REMOTE_FS_DIR=$(pwd)/$AGENT_NAME
+TOKEN="user:jenkinstoken"
+HOST="https://CONTROLLER_URL"
 
-#copy agent and create
-#java -jar jenkins-cli.jar -auth $TOKEN  -s $HOST get-node inbound1| java -jar jenkins-cli.jar -auth $TOKEN  -s $HOST create-node $AGENT_NAME
+##Download jenkins-cli
+#curl -O $HOST/jnlpJars/jenkins-cli.jar
+chmod a+x jenkins-cli.jar
 
+#copy agent node and create config on Controller
+#java -jar jenkins-cli.jar -auth $TOKEN  -s $HOST get-node inbound1| java -jar jenkins-cli.jar -auth $TOKEN  -s $HOST create-node inbound3
 
+#Create Agent node on Controller
 cat << EOF | java -jar jenkins-cli.jar -auth $TOKEN  -s $HOST create-node ${AGENT_NAME}
 <?xml version="1.1" encoding="UTF-8"?>
 <slave>
   <name>${AGENT_NAME}</name>
   <description></description>
-  <remoteFS>/home/ec2-user/${AGENT_NAME}</remoteFS>
+  <remoteFS>$REMOTE_FS_DIR</remoteFS>
   <numExecutors>1</numExecutors>
   <mode>NORMAL</mode>
   <retentionStrategy class="com.cloudbees.jenkins.plugins.replication.builds.ReplicatedRetentionStrategy" plugin="cloudbees-replication@1292"/>
   <launcher class="hudson.slaves.JNLPLauncher">
     <workDirSettings>
       <disabled>false</disabled>
-      <workDirPath>/home/ec2-user/${AGENT_NAME}</workDirPath>
+      <workDirPath>$REMOTE_FS_DIR</workDirPath>
       <internalDir>remoting</internalDir>
-      <failIfWorkDirIsMissing>true</failIfWorkDirIsMissing>
+      <failIfWorkDirIsMissing>false</failIfWorkDirIsMissing>
     </workDirSettings>
     <webSocket>true</webSocket>
   </launcher>
@@ -30,15 +35,25 @@ cat << EOF | java -jar jenkins-cli.jar -auth $TOKEN  -s $HOST create-node ${AGEN
 </slave>
 EOF
 
-#curl -O $HOST/jnlpJars/jenkins-cli.jar
-chmod a+x jenkins-cli.jar
-java -jar jenkins-cli.jar -auth $TOKEN  -s $HOST get-node ${AGENT_NAME}
+#Get created node config and display
+java -jar jenkins-cli.jar -auth $TOKEN -s $HOST get-node ${AGENT_NAME}
+echo "\n"
 
-
+#Get node secret
 #https://docs.cloudbees.com/docs/cloudbees-ci-kb/latest/client-and-managed-controllers/how-to-find-agent-secret-key
 #https://docs.cloudbees.com/docs/cloudbees-ci-kb/latest/client-and-managed-controllers/execute-groovy-with-a-rest-call
-echo 'jenkins.model.Jenkins.getInstance().getComputer("inbound1").getJnlpMac()' > agent_secret.groovy
-echo -n "AGENT SECRET FOR $AGENT_NAME"
-curl -XPOST --data-urlencode  "script=$(cat ./agent_secret.groovy)" -L --user $TOKEN $HOST/scriptText
+echo "jenkins.model.Jenkins.getInstance().getComputer('$AGENT_NAME').getJnlpMac()" > agent_secret.groovy
+AGENT_SECRET=$(curl -XPOST --data-urlencode  "script=$(cat ./agent_secret.groovy)" -L -s --user $TOKEN $HOST/scriptText)
+AGENT_SECRET=$(echo $AGENT_SECRET | sed "s#Result: ##g")
+echo  "AGENT SECRET FOR $AGENT_NAME : $AGENT_SECRET"
+
+#Create agent workspace
+mkdir -p $AGENT_NAME/remoting
+curl -sO $HOST/jnlpJars/agent.jar
+chmod a+x agent.jar
+#Launch agent
+java -jar agent.jar -jnlpUrl $HOST/computer/$AGENT_NAME/jenkins-agent.jnlp -secret $AGENT_SECRET -workDir "$REMOTE_FS_DIR" -failIfWorkDirIsMissing
+#Send in background,better create service lateron TODO
+#nohub java -jar agent.jar  ......   2>&1 & > /dev/null
 
 
