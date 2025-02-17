32c2ddbf501d756caa0762399a0193f3f0d3f0b5
cccaternberg <acaternberg@cloudbees.com>
2025-02-17 20:07:50 +0100

    update and cleanup

--- createSharedAgent.sh
+++ createSharedAgent.sh
@@ -5,6 +5,8 @@ export CJOC_URL="https://<CJOC_URL>"
 export SHARED_AGENT_NAME="mySharedAgent"
 export SHARED_AGENT_REMOTE_FS="/tmp"
 
+
+echo "------------------  RENDER SHARED_AGENT ITEM YAML ------------------"
 # We render the CasC template instances for the item (sharedAgent)
 # All variables  will be substituted
 cat << EOF > sharedAgent.yaml
@@ -32,21 +34,23 @@ items:
     sharedNodeRetentionStrategy: {}
 EOF
 
-
-# see https://docs.cloudbees.com/docs/cloudbees-ci-api/latest/bundle-management-api
 echo "------------------  CREATE/UPDATE SHARED AGENT ------------------"
+# see https://docs.cloudbees.com/docs/cloudbees-ci-api/latest/bundle-management-api
 curl -v -XPOST \
    --user $TOKEN \
    "${CJOC_URL}/casc-items/create-items" \
     -H "Content-Type:text/yaml" \
    --data-binary @sharedAgent.yaml
 
+echo "------------------  GET AGENT SECRET ------------------"
 #see https://docs.cloudbees.com/docs/cloudbees-ci-kb/latest/client-and-managed-controllers/how-to-find-agent-secret-key#_operations_center_shared_agents
 echo "def sharedAgent = Jenkins.getInstance().getItems(com.cloudbees.opscenter.server.model.SharedSlave.class).find { it.launcher != null && it.launcher.class.name == 'com.cloudbees.opscenter.server.jnlp.slave.JocJnlpSlaveLauncher' && it.name == '$SHARED_AGENT_NAME'}; return sharedAgent?.launcher.getJnlpMac(sharedAgent)" > agent_secret.groovy
 AGENT_SECRET=$(curl -XPOST --data-urlencode  "script=$(cat ./agent_secret.groovy)" -L -s --user $TOKEN $CJOC_URL/scriptText)
 AGENT_SECRET=$(echo $AGENT_SECRET | sed "s#Result: ##g")
 echo  "AGENT SECRET FOR $AGENT_NAME : $AGENT_SECRET"
 
+
+echo "------------------  START AGENT CONNECTION ------------------"
 #Download agent.jar and start agent connection to CJOC sharedAgent
 curl -sO $CJOC_URL/jnlpJars/agent.jar
 chmod a+x agent.jar
