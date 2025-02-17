d50e67b8ca1cea6a7fab0970925f827dbdef98e5
cccaternberg <acaternberg@cloudbees.com>
2025-02-17 15:04:46 +0100

    add

--- casc-reloadCjoc.sh
+++ casc-reloadCjoc.sh
@@ -2,10 +2,6 @@ C#! /bin/bash
 
 #https://docs.cloudbees.com/docs/cloudbees-ci-api/latest/bundle-management-api
 
-
-#Download jq, if not present
-#curl -sLo /tmp/jq https://stedolan.github.io/jq/download/linux64/jq
-#chmod 755 /tmp/jq
 curl -s -X GET -u $TOKEN ${CJOC_URL}/cjoc/casc-bundle-mgnt/check-bundle-update > check-bundle-update.json
 cat check-bundle-update.json | jq
 #updateAvailable=$(jq '."update-available"' check-bundle-update.json)
